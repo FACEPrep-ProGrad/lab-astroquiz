07e124d5a17e4fb49acd5f399658d872c9ae65ee
swati2904 <swati.lpu17@gmail.com>
2020-06-06 03:07:13 +0530

    FSJ0520A108

--- src/utility/EligibilityInterface.java
+++ src/utility/EligibilityInterface.java
@@ -1,1 +1,13 @@
+//PROGRESSION_3
+
+/*Create an interface EligibilityInterface in the utility package*
+*Create a method in interface - boolean checkUser (User user)*
+*Create a method in interface - boolean checkQuizAnswer (String points)*/
+
 package utility;
+import model.User;
+interface EligibilityInterface{
+	boolean checkUser (User user);
+	
+	boolean checkQuizAnswer (String points);
+}
